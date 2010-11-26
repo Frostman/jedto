fee93fa75066d18c6ea17a734299dcf28eb5a4a2
slukjanov <me@frostman.ru>
2010-11-26 12:12:03 +0300

    README, LICENSE and Apache license 2.0 copyrights added

--- src/main/java/ru/frostman/jedto/annotations/MapTo.java
+++ src/main/java/ru/frostman/jedto/annotations/MapTo.java
@@ -1,6 +1,20 @@
-package ru.frostman.jedto.annotations;
+/*
+ * Copyright 2010 Sergey "Frostman" Lukjanov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import ru.frostman.jedto.DTOMapper;
+package ru.frostman.jedto.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
